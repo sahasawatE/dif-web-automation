00b360037e65555501f2fe391d04edb8697a05c1
hok <hoklok3@gmail.com>
2022-09-29 16:47:52 +0700

    add new script and new test data

--- tests/smoketest/Bond.spec.js
+++ tests/smoketest/Bond.spec.js
@@ -55,7 +55,7 @@ test.describe('Smoketest | Bond - Bond Project', () => {
     expect(bn_title).toBe(bond_name)
     expect(issue_name).toBe(bond_name)
 
-    // insert data to bond
+    // funding objective information
     log.action('Select Submit Cooling Filing and Effective Filing Together')
     await page.locator('//input[@type="search"]').click()
     await page.locator('//div[@class="ant-select-item ant-select-item-option"]').click()
@@ -92,7 +92,7 @@ test.describe('Smoketest | Bond - Bond Project', () => {
         return cb.innerHTML
       })
     })
-    // console.log(cbs)
+
     for (let i = 0; i < cbs.length; i++) {
       if (i !== 10) {
         await page.locator('//input[@type="checkbox"]/parent::span/parent::label/span[2]').nth(i).scrollIntoViewIfNeeded()
@@ -120,15 +120,119 @@ test.describe('Smoketest | Bond - Bond Project', () => {
         log.success('Insert data into field secceed')
       }
     }
-    await page.waitForTimeout(6000)
+    await page.waitForTimeout(1000)
+
+    // issuer information
+    const issuer_track = page.locator('//span[@class="ant-radio ant-radio-checked"]').nth(1)
+    await issuer_track.scrollIntoViewIfNeeded()
+    await issuer_track.click();
+    log.action('Click radio button')
+
+    await page.locator('//input[@id="relevantLawsOnIncorporation"]').click()
+    const buz_under_th_law = page.locator('//div[@title="Business under Thai law."]')
+    await buz_under_th_law.scrollIntoViewIfNeeded()
+    await buz_under_th_law.click()
+    log.action('Click Business under Thai law.')
+
+    await page.locator('//input[@id="businessOrganizationUnderThaiLaw"]').click()
+    const bou_th = page.locator('//div[@title="Limited Company/ Public Company Limited"]')
+    await bou_th.scrollIntoViewIfNeeded()
+    await bou_th.click()
+    log.action('Click Limited Company/ Public Company Limited')
+
+    log.success('issuer information')
+
+    // business performance
+    await page.locator('//div[@class="tabCard"][2]').scrollIntoViewIfNeeded()
+    const issuer_type = page.locator('//input[@id="issuerType"]')
+    await issuer_type.scrollIntoViewIfNeeded()
+    await issuer_type.click()
+    await page.locator('//div[@title="Listed company"]').click()
+    log.success('business performance')
+
+    //issuer contact
+    const ceo_name = page.locator('//input[@id="ceoName"]')
+    await ceo_name.scrollIntoViewIfNeeded()
+    await ceo_name.fill(bp.init_issuer_profile.isser_info.issuer_contact.ceo_name)
+    log.action('Insert CEO name')
+
+    await page.locator('//input[@id="ceoPosition"]').fill(bp.init_issuer_profile.isser_info.issuer_contact.ceo_position)
+    await page.locator('//input[@id="ceoEmail"]').fill(bp.init_issuer_profile.isser_info.issuer_contact.ceo_email)
+    log.action('Insert CEO position')
+
+    const cfo_name = page.locator('//input[@id="cfoName"]')
+    await cfo_name.scrollIntoViewIfNeeded()
+    await cfo_name.fill(bp.init_issuer_profile.isser_info.issuer_contact.cfo_name)
+    log.action('Insert CFO name')
+
+    await page.locator('//input[@id="ceoPosition"]').fill(bp.init_issuer_profile.isser_info.issuer_contact.cfo_position)
+    await page.locator('//input[@id="ceoEmail"]').fill(bp.init_issuer_profile.isser_info.issuer_contact.cfo_enail)
+    log.action('Insert CFO position')
+
+    const issuer_duty_under_section_56 = page.locator('//input[@id="issuerDutiesUnderSection56"]')
+    await issuer_duty_under_section_56.scrollIntoViewIfNeeded()
+    await issuer_duty_under_section_56.click()
+    await page.locator('//div[@title="The company is obliged to submit financial statements and reports on financial position or the results of operations under Section 56."]').click()
+    log.action('Select The company is obliged to submit financial statements and reports on financial position or the results of operations under Section 56.')
+
+    await page.locator('//input[@id="standardTermsAndConditionFlag"]').click()
+    await page.locator(`//div[@title="Terms of the rights are subject to the example set forth on the SEC Office's website."]`).click()
+    log.action("Select Terms of the rights are subject to the example set forth on the SEC Office's website.")
+
+    const DL = page.locator('//div[@id="derivertiveLicense"]//span[@class="ant-radio"]')
+    await DL.scrollIntoViewIfNeeded()
+    await DL.click()
+    log.action('Click Radio button')
+
+    const qles = await page.$$eval('//div[@class="tabCard"][2]//div[@class="ql-editor ql-blank"]', els => {
+      return Array.from(els, el => el.getAttribute('data-placeholder'))
+    })
+
+    expect(qles.length).toEqual(bp.init_issuer_profile.isser_info.ql_editor_part_1.length)
+
+    let qles_i = 0
+
+    for (let qle of qles) {
+      log.action(qle)
+      const ql = page.locator(`//div[@class="tabCard"][2]//div[@data-placeholder="${qle}"]`)
+      await ql.scrollIntoViewIfNeeded()
+      await ql.click()
+      await ql.fill(bp.init_issuer_profile.isser_info.ql_editor_part_1[qles_i])
+      qles_i++
+      log.success('Insert data succeed')
+      await page.waitForTimeout(1000)
+    }
+    await page.waitForTimeout(1000)
+
+    const profileTH = page.locator('//input[@id="profileTH"]')
+    await profileTH.scrollIntoViewIfNeeded()
+    await profileTH.fill(bp.init_issuer_profile.isser_info.nature_of_buz.profile_th)
+    await page.locator('//input[@id="profileEN"]').fill(bp.init_issuer_profile.isser_info.nature_of_buz.profile_en)
+
+    const as_of = page.locator('//input[@id="majorShareholderPaidUpCapitalPercentageAsOf"]')
+    await as_of.scrollIntoViewIfNeeded()
+    await as_of.click()
+    await as_of.fill('2021-06-06')
+    await as_of.press('Enter')
+
+    log.action('Save draft')
+    const save_draft_btn = page.locator('//button[@type="submit"]').nth(1)
+    await save_draft_btn.scrollIntoViewIfNeeded()
+    await save_draft_btn.click()
+
+    await page.locator('//div[@class="ant-modal-body"]//button[1]').click()
+    await page.waitForTimeout(10000)
 
     log.action('Click back')
-    await page.locator('//*[@id="root"]/div/section/section/main/div/div/div/div/div[1]/div/div[1]/div/div/div[1]/div[1]/button').click()
+    const back_btn = page.locator('//*[@id="root"]/div/section/section/main/div/div/div/div/div[1]/div/div[1]/div/div/div[1]/div[1]/button')
+    await back_btn.scrollIntoViewIfNeeded()
+    await back_btn.click()
     await page.waitForTimeout(3000)
-    log.action('Delete bond')
-    await page.locator(`//td[@title="${bond_name}"]/parent::tr/td[16]/span/div/button[3]`).click()
-    log.action('Confitm delete bond')
-    await page.locator('//div[@class="ant-modal-confirm-btns"]/button[@class="ant-btn ant-btn-primary"][1]/span').click()
+
+    // log.action('Delete bond')
+    // await page.locator(`//td[@title="${bond_name}"]/parent::tr/td[16]/span/div/button[3]`).click()
+    // log.action('Confitm delete bond')
+    // await page.locator('//div[@class="ant-modal-confirm-btns"]/button[@class="ant-btn ant-btn-primary"][1]/span').click()
 
     await util.Logout()
   })
